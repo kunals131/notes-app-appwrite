ed2119d6a2b6c3dbc0353e21dd40d859e19bd6d6
Kunal  Sangtiani <86314140+kunals131@users.noreply.github.com>
2021-10-17 14:23:00 +0530

    correction : copyright name

--- src/pages/signin.page.jsx
+++ src/pages/signin.page.jsx
@@ -23,7 +23,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="/">
-        Noteitonline
+        OnlineNote
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
